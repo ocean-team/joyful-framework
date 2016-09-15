5a4e9ac49a91f1bb82f0092e12a5420a42299f8e
richey.liu <richey.liu@cnbexpress.com>
2016-09-15 15:11:03 +0800

    实现bean容器

---
+++ src/main/java/org/joyful4j/framework/helper/BeanHelper.java
@@ -0,0 +1,51 @@
+package org.joyful4j.framework.helper;
+
+import org.joyful4j.framework.utils.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Bean助手类(Bean容器)
+ * Created by richey on 16-9-15.
+ * @author richey
+ * @since 1.0.SNAPSHOT
+ */
+public class BeanHelper {
+    /**
+     * 定义Bean映射,用于存储bean类和bean实例的关系
+     */
+    private static final Map<Class<?>,Object> BEAN_MAP = new HashMap<Class<?>,Object>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass,obj);
+        }
+    }
+
+    /**
+     * 获取bean映射
+     * @return
+     */
+    public static Map<Class<?>,Object> getBeanMap(){
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取bean实例
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> cls){
+        if(!BEAN_MAP.containsKey(cls)){
+            throw new RuntimeException("can not get bean by class:"+cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+
+}
