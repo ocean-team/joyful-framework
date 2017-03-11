2b2cb3c4b566b40c44c317ddd2f52713471b15ab
richey <richey@localhost.richey>
2017-03-11 18:26:29 +0800

    有性能的日期工具类

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A lightweight MVC framework
 joyful是一个轻量级的mvc框架
 
 整合常用工具包,框架持续完善中...
+
+FIXME
+    @NotNull 注解未解析
---
+++ modules/utils/src/main/java/org/joyful4j/modules/utils/base/annotation/NotNull.java
@@ -0,0 +1,7 @@
+package org.joyful4j.modules.utils.base.annotation;
+
+/**
+ * Created by richey on 17-3-11.
+ */
+public @interface NotNull {
+}
